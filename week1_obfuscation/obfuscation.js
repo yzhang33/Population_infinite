241dee2b92dffb0ffba55c071d8d37cf1fc513cf
yzhang33 <youmingzhang19@gmail.com>
2020-09-30 10:11:10 -0400

    files

--- week1_obfuscation/obfuscation.js
+++ week1_obfuscation/obfuscation.js
@@ -1,6 +1,8 @@
 import puppeteer from 'puppeteer';
 import randomWords from 'random-words'
 
+
+
 var watchHistory = [];
 const browser = await puppeteer.launch({headless: false});
 
@@ -10,7 +12,7 @@ await page.setViewport({width: 1280, height: 720});
 
 console.log('going to youtube')
 await page.goto('https://www.youtube.com');
-await page.waitForSelector('#avatar-btn');
+//await page.waitForSelector('#avatar-btn');
 await page.click('form');
 
 const randomSearch = randomWords({ exactly:5, min: 3, max: 10 });
@@ -26,19 +28,16 @@ console.log('found video, opening the first one.')
 // click the link for the first video appear.
 await page.click('a#video-title');
 var count = 0;
-while(count<10) {
-    await page.waitFor('h1 yt-formatted-string');
-
+while(count<100) {
+    await page.waitForSelector('h1 yt-formatted-string');
+    
     const videoTitle = await page.evaluate(function() {
     return document.querySelector('h1 yt-formatted-string').innerText;
     });
     var duration = Math.floor((Math.random()*3)+1);
     watchHistory.push({
-        title: videoTitle,
-        watchedDuration:duration
+        "title": videoTitle
     });
-    console.log('watching video ' + videoTitle);
-
     // watch the video for the watchDuration seconds
     await page.waitFor(duration * 1000);
 
@@ -47,6 +46,6 @@ while(count<10) {
     await page.click('#related #items #contents a');
     count++ ;
 }
-
 console.log(watchHistory);
+
 await browser.close();
